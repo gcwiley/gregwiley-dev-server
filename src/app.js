e0272cf6e3202534636156816dc5194655d78642
Greg Wiley <gcwiley@gmail.com>
2023-08-06 12:05:15 -0400

    additional code added

--- src/app.js
+++ src/app.js
@@ -21,7 +21,6 @@ import './db/db.js';
 
 // import the routers
 import { projectRouter } from './routes/project.js';
-import { postRouter } from './routes/post.js';
 
 // create an express instance
 const app = express();
@@ -41,7 +40,6 @@ app.use(logger('dev'));
 
 // register the routers
 app.use(projectRouter);
-app.use(postRouter);
 
 // handle all other routes with angular app - returns angular app
 app.get('*', (req, res) => {
